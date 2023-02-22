96c7cb7864d90d06a246cf0aad246752f80e3dc0
Dali_Meftah <61090619+ReYAp047@users.noreply.github.com>
2023-02-22 03:41:45 +0100

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn municipaliteDashboard.wsgi --log-file -
--- municipaliteDashboard/settings.py
+++ municipaliteDashboard/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#!l9eguz13h7-_zmg$d4qnft&334w0y(406-ut^mxmy_*h47!p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -134,6 +134,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
